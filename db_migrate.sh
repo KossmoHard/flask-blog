f0a1c14c5fc890699fd581c7c2d99ac53b10cc16
KossmoHard <zvladsz@gmail.com>
2019-06-05 16:26:50 +0300

    Commit Shell

--- db_migrate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source .venv/bin/activate
-python app/manage.py db init
-python app/manage.py db migrate
-python app/manage.py db upgrade
--- virtualenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip install virtualenv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
